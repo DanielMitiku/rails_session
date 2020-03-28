f3de807af619fe9e1a090ada027194001957e4d7
DanielMitiku <wedanielmitiku@gmail.com>
2020-03-28 12:35:53 +0300

    update navbar and users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:show]
+
   def show
     @user = User.find(params[:id])
   end
@@ -10,6 +12,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "Welcome to the app!"
       redirect_to @user
     else
